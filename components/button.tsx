ab7b2912bf82a354d332c4afd0725020f6a80dfe
Erick van der Linde <erick@andile.io>
2022-08-13 21:11:47 +0200

    Add new SecondaryButton

--- components/button.tsx
+++ components/button.tsx
@@ -2,7 +2,15 @@ import React from "react";
 
 export const Button = ({ children }: { children: React.ReactNode }) => {
   return (
-    <button className={"border border-black p-1 hover:ring ring-black whitespace-nowrap"}>
+    <button className={"border border-black p-2 hover:ring ring-black whitespace-nowrap"}>
+      {children}
+    </button>
+  );
+};
+
+export const SecondaryButton = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <button className={"p-2 hover:ring ring-black whitespace-nowrap"}>
       {children}
     </button>
   );
